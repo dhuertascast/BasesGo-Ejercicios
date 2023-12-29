be8650e64e434e7358d4a54ba8dc5537e1eb7f44
dhuertascast <david.huertascastellanos@mercadolibre.com.co>
2023-12-29 17:30:52 -0500

    foutrh excersise

---
+++ 04-Ejercicio Variables/dato.go
@@ -0,0 +1,10 @@
+
+var lastName string = "Smith" // Correcto
+
+var age int = "35" // incorrecto si declara un entero no debe ponerle comillas
+
+boolean := "false" // incorrecto de nuevo sobran las comillas
+
+var salary string = 45857.90 // incorrecto declara un string y asigna un float
+
+var firstName string = "Mary" // Correcto
